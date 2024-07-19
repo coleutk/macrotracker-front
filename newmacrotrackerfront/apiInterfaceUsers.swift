7f6bcb67cdfbdb6576b6203702068964bb29596f
coleutk <colepricecollege@gmail.com>
2024-07-19 16:20:19 -0500

    Added EditProfileView to Change User Details + Added to ProfileAPI

--- newmacrotrackerfront/apiInterfaceUsers.swift
+++ newmacrotrackerfront/apiInterfaceUsers.swift
@@ -5,6 +5,11 @@ struct User: Identifiable, Codable {
     var selectedGoal: SelectedGoal?
 }
 
+struct UserDetails: Codable {
+    var username: String
+    var email: String
+}
+
 struct SelectedGoal: Codable {
     var id: String // Use 'id' instead of '_id' to be consistent
     var name: String
@@ -152,6 +157,156 @@ func userLogin(email: String, password: String, completion: @escaping (Bool, Str
             }
         } catch {
             completion(false, "Failed to parse response")
+        }
+    }
+    
+    task.resume()
+}
+
+
+// Pull Username/E-Mail (USER)
+func getUserDetails(_ completion: @escaping (Result<UserDetails, Error>) -> Void) {
+    guard let token = UserDefaults.standard.string(forKey: "token") else {
+        completion(.failure(NSError(domain: "", code: 401, userInfo: [NSLocalizedDescriptionKey: "User not authenticated"])))
+        return
+    }
+    
+    guard let url = URL(string: "http://localhost:3000/users/me") else {
+        completion(.failure(NSError(domain: "", code: 400, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
+        return
+    }
+
+    var request = URLRequest(url: url)
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization") // Include the token
+    request.httpMethod = "GET"
+    
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        guard let data = data, error == nil else {
+            completion(.failure(error ?? NSError(domain: "", code: 400, userInfo: [NSLocalizedDescriptionKey: "Network error"])))
+            return
+        }
+        
+        do {
+            if let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any],
+               let user = json["user"] as? [String: Any],
+               let username = user["username"] as? String,
+               let email = user["email"] as? String{
+                
+                let userDetails = UserDetails(
+                    username: username,
+                    email: email
+                )
+                
+                //print("Selected Goal parsed: \(userDetails)")
+                completion(.success(userDetails))
+            } else {
+                completion(.failure(NSError(domain: "", code: 400, userInfo: [NSLocalizedDescriptionKey: "Invalid response from server"])))
+            }
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    
+    task.resume()
+}
+
+
+// Update Username/E-Mail (USER)
+func updateUserDetails (username: String, email: String, completion: @escaping (Result<Void, Error>) -> Void) {
+    guard let token = UserDefaults.standard.string(forKey: "token") else {
+        completion(.failure(NSError(domain: "", code: 401, userInfo: [NSLocalizedDescriptionKey: "User not authenticated"])))
+        return
+    }
+    
+    var request = URLRequest(url: URL(string: "http://localhost:3000/users/updateUser")!)
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization") // Include the token
+    request.httpMethod = "PATCH"
+    
+    let body: [String: Any] = [
+        "username": username,
+        "email": email
+    ]
+    
+    request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
+    
+    
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        if let error = error {
+            DispatchQueue.main.async {
+                completion(.failure(error))
+            }
+            return
+        }
+        
+        guard let data = data, let httpResponse = response as? HTTPURLResponse else {
+            DispatchQueue.main.async {
+                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid response"])))
+            }
+            return
+        }
+        
+        if httpResponse.statusCode == 200 {
+            DispatchQueue.main.async {
+                completion(.success(()))
+            }
+        } else {
+            let responseString = String(data: data, encoding: .utf8) ?? "Unknown error"
+            DispatchQueue.main.async {
+                completion(.failure(NSError(domain: "", code: httpResponse.statusCode, userInfo: [NSLocalizedDescriptionKey: responseString])))
+            }
+        }
+    }
+    
+    task.resume()
+}
+
+
+// Update Password in Edit Profile (USER)
+func updateUserPassword(currentPassword: String, newPassword: String, confirmPassword: String, completion: @escaping (Result<Void, Error>) -> Void) {
+    guard let token = UserDefaults.standard.string(forKey: "token") else {
+        completion(.failure(NSError(domain: "", code: 401, userInfo: [NSLocalizedDescriptionKey: "User not authenticated"])))
+        return
+    }
+    
+    var request = URLRequest(url: URL(string: "http://localhost:3000/users/updatePassword")!)
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+    request.httpMethod = "PATCH"
+    
+    let body: [String: Any] = [
+        "currentPassword": currentPassword,
+        "newPassword": newPassword,
+        "confirmPassword": confirmPassword
+    ]
+    
+    request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
+    
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        if let error = error {
+            DispatchQueue.main.async {
+                completion(.failure(error))
+            }
+            return
+        }
+        
+        guard let data = data, let httpResponse = response as? HTTPURLResponse else {
+            DispatchQueue.main.async {
+                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid response"])))
+            }
+            return
+        }
+        
+        if httpResponse.statusCode == 200 {
+            DispatchQueue.main.async {
+                completion(.success(()))
+            }
+        } else {
+            let responseString = String(data: data, encoding: .utf8) ?? "Unknown error"
+            DispatchQueue.main.async {
+                completion(.failure(NSError(domain: "", code: httpResponse.statusCode, userInfo: [NSLocalizedDescriptionKey: responseString])))
+            }
         }
     }
     
--- swiftfiles/ProfileView.swift
+++ swiftfiles/ProfileView.swift
@@ -59,6 +59,14 @@ struct ProfileView: View {
                         }
                         .listRowBackground(Color(red: 20/255, green: 20/255, blue: 30/255))
                         .padding(.vertical, 10)
+                        
+                        NavigationLink (destination: EditProfileView()){
+                            Text("Edit Profile")
+                                .font(.headline)
+                                .foregroundColor(.white.opacity(0.70))
+                        }
+                        .listRowBackground(Color(red: 20/255, green: 20/255, blue: 30/255))
+                        .padding(.vertical, 10)
                     }
                     .listStyle(PlainListStyle())
                     .background(Color(red: 44/255, green: 44/255, blue: 53/255))
@@ -89,6 +97,225 @@ struct ProfileView: View {
     }
 }
 
-#Preview {
-    ProfileView(username: "cratik") // Hardcoded temp
+struct EditProfileView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var userDetails: UserDetails? = nil
+    @State private var errorMessage: String? = nil
+    
+    @State private var username: String = ""
+    @State private var email: String = ""
+    @State private var currentPassword: String = ""
+    @State private var newPassword: String = ""
+    @State private var confirmPassword: String = ""
+    
+    @State private var passwordErrorMessage: String? = nil
+    @State private var alertMessage: String? = nil
+    @State private var showAlert = false
+    
+    var body: some View {
+        ZStack {
+            Color(red: 20/255, green: 20/255, blue: 30/255)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Edit Details:")
+                    .font(.title2)
+                    .bold()
+                    .foregroundColor(.white.opacity(0.70))
+                
+                if userDetails != nil {
+                    HStack {
+                        MacroDisplayVertical(nutrient: "Name", color: Color(.white))
+                        
+                        TextField("Enter Username...", text: $username)
+                            .padding(14)
+                            .frame(height: 60)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.black.opacity(0.20))
+                            .cornerRadius(15)
+                            .padding(.trailing, 22)
+                            .padding(.leading, -10)
+                    }
+                    .padding(3)
+                }
+                
+                HStack {
+                    MacroDisplayVertical(nutrient: "E-Mail", color: Color(red: 10/255, green: 211/255, blue: 255/255))
+                    
+                    TextField("Enter E-Mail...", text: $email)
+                        .padding(14)
+                        .frame(height: 60)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.black.opacity(0.20))
+                        .cornerRadius(15)
+                        .padding(.trailing, 22)
+                        .padding(.leading, -10)
+                }
+                .padding(3)
+                
+                Button(action: {
+                    // Save changes here
+                    updateUser()
+                    
+                    alertMessage = "Information updated successfully!"
+                    showAlert = true
+                }) {
+                    Text("Confirm Changes")
+                        .foregroundColor(.white.opacity(0.70))
+                        .padding(14)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue.opacity(0.50))
+                        .cornerRadius(15)
+                        .padding(.horizontal, 22)
+                        .padding(.top, 20)
+                }
+                
+                .padding(.bottom, 45)
+                
+                Text("Change Password:")
+                    .font(.title2)
+                    .bold()
+                    .foregroundColor(.white.opacity(0.70))
+                
+                HStack {
+                    MacroDisplayVertical(nutrient: "Current", color: Color(.white))
+                    
+                    SecureField("Current Password", text: $currentPassword)
+                        .padding(14)
+                        .frame(height: 60)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.black.opacity(0.20))
+                        .cornerRadius(15)
+                        .padding(.trailing, 22)
+                        .padding(.leading, -10)
+                }
+                .padding(3)
+                
+                HStack {
+                    MacroDisplayVertical(nutrient: "New", color: Color(red: 10/255, green: 211/255, blue: 255/255))
+                    
+                    SecureField("Current Password", text: $newPassword)
+                        .padding(14)
+                        .frame(height: 60)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.black.opacity(0.20))
+                        .cornerRadius(15)
+                        .padding(.trailing, 22)
+                        .padding(.leading, -10)
+                }
+                .padding(3)
+                
+                HStack {
+                    MacroDisplayVertical(nutrient: "Confirm", color: Color(red: 46/255, green: 94/255, blue: 170/255))
+                    
+                    SecureField("Current Password", text: $confirmPassword)
+                        .padding(14)
+                        .frame(height: 60)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.black.opacity(0.20))
+                        .cornerRadius(15)
+                        .padding(.trailing, 22)
+                        .padding(.leading, -10)
+                }
+                .padding(3)
+                
+                
+                Button(action: {
+                    // Save changes here
+                    updatePassword()
+                    alertMessage = "Password updated successfully!"
+                    showAlert = true
+                    //presentationMode.wrappedValue.dismiss()
+                }) {
+                    Text("Confirm Changes")
+                        .foregroundColor(.white.opacity(0.70))
+                        .padding(14)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue.opacity(0.50))
+                        .cornerRadius(15)
+                        .padding(.horizontal, 22)
+                        .padding(.top, 20)
+                }
+                
+                if let passwordErrorMessage = passwordErrorMessage {
+                    Text(passwordErrorMessage)
+                        .foregroundColor(.red)
+                        .padding()
+                }
+            }
+            .foregroundColor(.white.opacity(0.70))
+            .padding(.top, -90)
+        }
+        .onAppear {
+            fetchUserDetails()
+        }
+        .alert(isPresented: $showAlert) { // Show confirmation alert
+            Alert(
+                title: Text(alertMessage ?? ""),
+                dismissButton: .default(Text("OK")) {
+                    // Dismiss the view if needed
+                    presentationMode.wrappedValue.dismiss()
+                }
+            )
+        }
+    }
+    
+    private func fetchUserDetails() {
+        getUserDetails { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let userDetails):
+                    self.userDetails = userDetails
+                    self.username = userDetails.username
+                    self.email = userDetails.email
+                case .failure(let error):
+                    self.errorMessage = error.localizedDescription
+                }
+            }
+        }
+    }
+    
+    func updateUser() {
+        updateUserDetails(username: username, email: email) { result in
+            switch result {
+            case .success:
+                self.errorMessage = nil
+                
+            case .failure(let error):
+                self.errorMessage = error.localizedDescription
+            }
+            
+        }
+    }
+    
+    func updatePassword() {
+        guard !currentPassword.isEmpty, !newPassword.isEmpty, !confirmPassword.isEmpty else {
+            passwordErrorMessage = "All password fields are required"
+            return
+        }
+        
+        guard newPassword == confirmPassword else {
+            passwordErrorMessage = "New passwords do not match"
+            return
+        }
+        
+        passwordErrorMessage = nil
+        
+        updateUserPassword(currentPassword: currentPassword, newPassword: newPassword, confirmPassword: confirmPassword) { result in
+            switch result {
+            case .success:
+                //self.passwordErrorMessage = "Password updated successfully!"
+                presentationMode.wrappedValue.dismiss()
+            case .failure(let error):
+                self.passwordErrorMessage = error.localizedDescription
+            }
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView(username: "cratik")
+    }
 }
